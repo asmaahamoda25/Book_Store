0a359b298558543d2d4e589c8c9474ae81ab54f1
RehamHeshamx <144820776+RehamHeshamx@users.noreply.github.com>
2025-11-22 05:39:21 +0200

    DAO

---
+++ src/main/java/com/mycompany/bookstore/daoImp/BookDAOimp.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.bookstore.daoImp;
+import com.mycompany.bookstore.dao.BookDAO;
+import com.mycompany.bookstore.model.Book;
+import java.sql.*;
+/**
+ *
+ * @author Pc
+ */
+public class BookDAOimp implements BookDAO {
+    private Connection connection;
+
+    public BookDAOimp(Connection connection) {
+        this.connection = connection;
+    }
+    
+    
+    @Override
+    public void addBook(Book book) {
+        String sql = "INSERT INTO Books(bookId, title, author, genre, publicationDate, "
+                + "price, quantityInStock, description, coverImageURL) "
+                + "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
+        try (PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setString(1, book.getBookId());
+            statement.setString(2, book.getTitle());
+            statement.setString(3, book.getAuthor());
+            statement.setString(4, book.getGenre());
+            statement.setDate(5, Date.valueOf(book.getPublicationDate()));
+            statement.setBigDecimal(6, book.getPrice());
+            statement.setInt(7, book.getQuantityInStock());
+            statement.setString(8, book.getDescription());
+            statement.setString(9, book.getCoverImageURL());
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
--- src/main/java/com/mycompany/bookstore/util/DBConnection.java
+++ src/main/java/com/mycompany/bookstore/util/DBConnection.java
@@ -5,7 +5,8 @@ import java.sql.SQLException;
 
 // url for most ms sql server is "jdbc:sqlserver:hostname\\instance:port;databseName=dbName"
 public class DBConnection { 
-    private static final String url = "jdbc:sqlserver://REHAM-PC\\SQLEXPRESS:1433;databaseName=Book_Store;encrypt=true;trustServerCertificate=true";
+    private static final String url = "jdbc:sqlserver://REHAM-PC\\SQLEXPRESS"
+            + ":1433;databaseName=Book_Store;encrypt=true;trustServerCertificate=true";
     private static final String user = "Book_StoreDB";
     private static final String password = "12345";
 
